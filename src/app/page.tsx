246f43a049d466fb57b04e548a86a33379cb168a
gavin hemsada <69586429+GavinHemsada@users.noreply.github.com>
2026-02-21 20:34:54 +0530

    Change redirect path after database connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,13 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import connectToDatabase from "@/(backend)/utility/db"; // Ensure correct import path
 
 export default function DashboardPage() {
   const router = useRouter(); // useRouter must be inside a function component
 
   useEffect(() => {
     async function init() {
-      await connectToDatabase(); // Call the async function inside useEffect
-      router.push("/admin/Login"); // Redirect after connection
+      router.push("/admin/dashboard"); // Redirect after connection
     }
 
     init(); // Invoke the async function
